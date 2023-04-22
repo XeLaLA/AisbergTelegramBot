1571429972f14841f5779d62718818779490b195
Alex <laxelaalexla@gmail.com>
2023-04-22 11:04:36 +0300

    Заготовка для telegram bot

---
+++ src/main/java/com/example/AisbergTelegramBot/Bot.java
@@ -0,0 +1,101 @@
+package com.example.AisbergTelegramBot;
+
+import com.example.AisbergTelegramBot.components.BotCommands;
+import com.example.AisbergTelegramBot.components.Buttons;
+import com.example.AisbergTelegramBot.config.BotConfig;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+
+@Slf4j
+@Component
+public class Bot extends TelegramLongPollingBot implements BotCommands {
+     final BotConfig config;
+
+    public Bot(BotConfig config) {
+        this.config = config;
+        try {
+            this.execute(new SetMyCommands(LIST_OF_COMMANDS, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e){
+            log.error(e.getMessage());
+        }
+    }
+    @Override
+    public String getBotUsername() { return config.getBotName(); }
+    @Override
+    public String getBotToken() { return config.getToken(); }
+    @Override
+    public void onUpdateReceived(@NotNull Update update) {
+        long chatId = 0;
+        long userId = 0;
+        String userName = null;
+        String receivedMessage;
+
+        //если получено сообщение текстом
+        if(update.hasMessage()) {
+            chatId = update.getMessage().getChatId();
+            userId = update.getMessage().getFrom().getId();
+            userName = update.getMessage().getFrom().getFirstName();
+
+            if (update.getMessage().hasText()) {
+                receivedMessage = update.getMessage().getText();
+                botAnswerUtils(receivedMessage, chatId, userName);
+            }
+
+            //если нажата одна из кнопок бота
+        } else if (update.hasCallbackQuery()) {
+            chatId = update.getCallbackQuery().getMessage().getChatId();
+            userId = update.getCallbackQuery().getFrom().getId();
+            userName = update.getCallbackQuery().getFrom().getFirstName();
+            receivedMessage = update.getCallbackQuery().getData();
+
+            botAnswerUtils(receivedMessage, chatId, userName);
+        }
+    }
+    private void botAnswerUtils(String receivedMessage, long chatId, String userName) {
+        switch (receivedMessage){
+            case "/start":
+                startBot(chatId, userName);
+                break;
+            case "/help":
+                sendHelpText(chatId, HELP_TEXT);
+                break;
+            default: break;
+        }
+    }
+
+    private void startBot(long chatId, String userName) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Приветствуем от лица ООО Айсберг, " + userName + "! Я Telegram bot.");
+        message.setReplyMarkup(Buttons.inlineMarkup());
+
+        try {
+            execute(message);
+            log.info("Reply sent");
+        } catch (TelegramApiException e){
+            log.error(e.getMessage());
+        }
+    }
+
+    private void sendHelpText(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+            log.info("Reply sent");
+        } catch (TelegramApiException e){
+            log.error(e.getMessage());
+        }
+    }
+}
